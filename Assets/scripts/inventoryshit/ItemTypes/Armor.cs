7c19b20b1529aacee4c9cf8facd736dbd0dd0b6c
JalapenosBud <Jalapenosbud@gmail.com>
2018-03-21 09:55:55 +0100

    enums

---
+++ Assets/scripts/inventoryshit/ItemTypes/Armor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Armor {
+    Head,
+    Neck,
+    Chest,
+    Gloves,
+    Legs,
+    Boots,
+    Weapon	
+}
---
+++ Assets/scripts/inventoryshit/ItemTypes/Consumables.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Consumables { 
+    Apple,
+    Water,
+    Potion_Health
+}
