590c87bdc6c1a324cc59dc1ef3a1271173b0d727
JalapenosBud <Jalapenosbud@gmail.com>
2018-03-24 20:42:24 +0100

    create char equip panel

--- Assets/scripts/inventoryshit/Inventory/MainInventory.cs
+++ Assets/scripts/inventoryshit/Inventory/MainInventory.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MainInventory  : MonoBehaviour{
 
-    public GameObject slotspanel;
+    public GameObject bagSlotsPanel;
+    public GameObject charSlotsPanel;
     private SlotIncrementer slotIncrementer;
 
-    private GOSlot[] slots;
+    private GOSlot[] bagSlots;
+
+    private GOSlot[] characterSlots;
+
+    private List<GOSlot> allSlots = new List<GOSlot>();
 
     private Item tmpItem;
     private Item tmpItemOtherSlot;
@@ -24,6 +32,7 @@ public class MainInventory  : MonoBehaviour{
         inventoryDatabase = new InventoryDatabase();
         slotIncrementer = new SlotIncrementer();
 
+        CreateCharacterEquipSlots();
         CreateBagSlots();
 
         //TODO: fixme; for now: dummy code 
@@ -43,12 +52,12 @@ public class MainInventory  : MonoBehaviour{
 
     private void AddItemToSlot(string name)
     {
-        while( i <= slots.Length)
+        while( i <= bagSlots.Length)
         {
-            if(!slots[i].slot.DoWeContainAnItem())
+            if(!bagSlots[i].slot.DoWeContainAnItem())
             {
-                slots[i].slot.AssignSlotRefID(LookUpItem(name));
-                print("name: " + slots[i].slot.GetItem().GetName() + " SlotRefID " + slots[i].slot.GetItem().SlotRefID);
+                bagSlots[i].slot.AssignSlotRefID(LookUpItem(name));
+                print("name: " + bagSlots[i].slot.GetItem().GetName() + " SlotRefID " + bagSlots[i].slot.GetItem().SlotRefID);
                 i++;
                 
                 break;
@@ -94,14 +103,14 @@ public class MainInventory  : MonoBehaviour{
         item = tmpItem;
 
         //access GOSLOT array at the old index
-        slots[tmpItem.SlotRefID].slot.UpdateItemIDAtSlot(tmpItemOtherSlot);
+        bagSlots[tmpItem.SlotRefID].slot.UpdateItemIDAtSlot(tmpItemOtherSlot);
 
         //update item at the new slot
         slot.UpdateItemIDAtSlot(item);
 
         //assign swapped images properly
-        slots[tmpItem.SlotRefID].GetComponent<Image>().sprite = tmpItem.GetSprite();
-        slots[tmpItemOtherSlot.SlotRefID].GetComponent<Image>().sprite = tmpItemOtherSlot.GetSprite();
+        bagSlots[tmpItem.SlotRefID].GetComponent<Image>().sprite = tmpItem.GetSprite();
+        bagSlots[tmpItemOtherSlot.SlotRefID].GetComponent<Image>().sprite = tmpItemOtherSlot.GetSprite();
 
 
     }
@@ -114,30 +123,72 @@ public class MainInventory  : MonoBehaviour{
         }
     }
 
-
+    //CALL THIS SECOND #2
     //refactor: fit to all kind of containers
     private void CreateBagSlots()
     {
         //make a root "node"
         //assign it values
-        slots = slotspanel.GetComponentsInChildren<GOSlot>();
-        slots[0].slot = new BagSlot();
-        slots[0].slot.ID = 0;
-        Debug.Log("root ID " + slots[0].slot.ID);
+        
+        bagSlots = bagSlotsPanel.GetComponentsInChildren<GOSlot>();
+        bagSlots[0].slot = new BagSlot();
+        bagSlots[0].slot.ID = 0;
+        Debug.Log("root ID " + bagSlots[0].slot.ID);
         //set the id to be incremented
-        slotIncrementer.counter = slots[0].slot.ID;
-        for (int i = 1; i < slots.Length; i++)
+        slotIncrementer.counter = bagSlots[0].slot.ID;
+        for (int i = 1; i < bagSlots.Length; i++)
         {
             //make new slot
-            slots[i].slot = new BagSlot();
+            bagSlots[i].slot = new BagSlot();
             //increment the counter for each new instantiation
             slotIncrementer.Increment(slotIncrementer.counter);
             //now set the ID to the newly incremented counter
             //but only for this object
-            slots[i].slot.ID = slotIncrementer.counter;
-            Debug.Log("slot: " + slots[i].slot.ID);
+            bagSlots[i].slot.ID = slotIncrementer.counter;
+            Debug.Log("slot: " + bagSlots[i].slot.ID);
             //Debug.Log("CI: " + createIncrement.counter);
         }
+    }
+    /*
+     * character slot start at 0
+     * bagslot starts at where characterslot ended
+     * keep id in the same array
+     * 
+     */ 
+     //CALL THIS FIRST #1
+    private void CreateCharacterEquipSlots()
+    {
+        if (charSlotsPanel == null)
+        {
+            return;
+        }
+        characterSlots = bagSlotsPanel.GetComponentsInChildren<GOSlot>();
+        characterSlots[0].slot = new BagSlot();
+        characterSlots[0].slot.ID = 0;
+        Debug.Log("root ID " + characterSlots[0].slot.ID);
+        //set the id to be incremented
+        slotIncrementer.counter = characterSlots[0].slot.ID;
+        for (int i = 1; i < bagSlots.Length; i++)
+        {
+            //make new slot
+            foreach (EnumArmor eee in Enum.GetValues(typeof (EnumArmor)))
+            {
+                characterSlots[i].slot = new CharacterSlot(eee);
+                break;
+            }
+            
+            //increment the counter for each new instantiation
+            slotIncrementer.Increment(slotIncrementer.counter);
+            //now set the ID to the newly incremented counter
+            //but only for this object
+            characterSlots[i].slot.ID = slotIncrementer.counter;
+            Debug.Log("slot: " + characterSlots[i].slot.ID);
+            //Debug.Log("CI: " + createIncrement.counter);
+        }
+
+
+        //must be called in the end to sum id up
+        allSlots.AddRange(characterSlots);
     }
 
 
